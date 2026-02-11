e2e158d51cfc183dbab93d4c8a723a64d33886a0
Ju2li <Julie.dankovicova@seznam.cz>
2026-02-11 11:45:48 +0100

    ok
    oki

---
+++ cviceni 3.3.2/script.js
@@ -0,0 +1,9 @@
+userName = "Zita";
+userName2 = "Klárka";
+
+let userNameAge = 17;
+let userName2Age = 16;
+
+let userNameAgeTotal = userNameAge + userName2Age;
+
+console.log("Součet věků lidí " + userName + " a " + userName2 + " je " + userNameAgeTotal)
