22653c7127785cb879c9eb39c954d535ef4cc20d
Ju2li <Julie.dankovicova@seznam.cz>
2026-03-04 11:40:28 +0100

    yas queen
    ok

--- 3.3.3/script.js
+++ 3.3.3/script.js
@@ -1,6 +1,6 @@
-let age = 21;
+let age = 20;
 
-if (age > 20) {
+if (age >= 20) {
     console.log("Věk je větší než 20.");
 } else {
     console.log("Věk je 20 nebo méně")
---
+++ 3.3.4 cvičení/script.js
@@ -0,0 +1,10 @@
+let age = 3;
+
+if (age <= 3) {
+    console.log("Vstup zdarma!")
+    if (age >3 && age >10) {
+        console.log("Dostanete 30% slevu!")
+    } else {
+        console.log("Platíš plnou cenu.")
+    }
+}
